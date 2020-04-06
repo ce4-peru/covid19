705b7ea0a60b3ba1de6e5c55866afa039859268f
MicaPte <micaela.delapuente@upch.pe>
2020-04-06 15:33:09 -0500

    mapa leyenda buena

--- Scripts/COVID_REGIONS.R
+++ Scripts/COVID_REGIONS.R
@@ -40,6 +40,7 @@ unique(sh_peru$NAME_1)
 sh_peru$NAME_1 <- factor(sh_peru$NAME_1)
 levels(sh_peru@data$NAME_1)
 
+
 # Change names
 str(sh_peru@data)
 levels(sh_peru$NAME_1) <- c(levels(sh_peru$NAME_1), 
@@ -77,22 +78,35 @@ unique(sh_peru@data$NAME_1)
 # [21] "PUNO"          "SAN MARTIN"    "TACNA"         "TUMBES"       
 # [25] "UCAYALI"  
 
+########################################################################
+# Cuando haya tiempo
+# Unir lo que era Lima province (fila 15) con Lima región (fila 16)
+plot(sh_peru[16,]) 
+plot(sh_peru[15,], add=T, col="red") 
+plot(sh_peru[16,], add=T, col="green") 
+#########################################################################
+
 ## Check data of cases
 names(caso_covid)
 colnames(caso_covid)[1] <- "CE4_ID"
 colnames(caso_covid)[7] <- "NAME_1"
-unique(caso_covid$NAME_1)
-# [1] "LIMA"          "AREQUIPA"      "HUANUCO"       "ICA"          
-# [5] "CUSCO"         "ANCASH"        "CALLAO"        "LA LIBERTAD"  
-# [9] "LAMBAYEQUE"    "PIURA"         "LORETO"        "MADRE DE DIOS"
-# [13] "SAN MARTIN"    "JUNIN"         "TUMBES"        "CAJAMARCA"    
-# [17] "PASCO"         "AYACUCHO"      "TACNA"         "HUANCAVELICA" 
-# [21] "APURIMAC"
 
-# Eliminar NAs
+# Checar las variables. Que no haya tildes, espacios innecesarios o ""
+unique(caso_covid$NAME_1)
+# Sale mal Huanuco
+
+# Eliminar NAs y tildes
 #caso_covid <- caso_covid[caso_covid$NAME_1 != ""]
-#caso_covid$NAME_1[caso_covid$Caso_ID %in% c(10,11)] <- "Huanuco"
-#caso_covid$NAME_1[caso_covid$NAME_1 == "La libertad"] <- "La Libertad"
+caso_covid$NAME_1[caso_covid$Caso_ID %in% c(10,11)] <- "HUANUCO"
+# no funciona no s[e por que]
+caso_covid[11,7] <- "HUANUCO"
+caso_covid[10,7] <- "HUANUCO"
+unique(caso_covid$NAME_1)
+# [1] "LIMA"          "AREQUIPA"      "HUANUCO"       "ICA"           "CUSCO"        
+# [6] "ANCASH"        "CALLAO"        "LA LIBERTAD"   "LAMBAYEQUE"    "PIURA"        
+# [11] "LORETO"        "MADRE DE DIOS" "SAN MARTIN"    "JUNIN"         "TUMBES"       
+# [16] "CAJAMARCA"     "PASCO"         "AYACUCHO"      "TACNA"         "HUANCAVELICA" 
+# [21] "APURIMAC"
 
 casos_region <- data.frame(table(caso_covid$NAME_1))
 colnames(casos_region)[1] <- "REGION"
@@ -102,6 +116,11 @@ colnames(casos_region)[2] <- "NUMERO_CASOS"
 names(pob_region) # "REGION"    "POBLACION"
 pob_region[[1]] <- toupper(pob_region[[1]])
 unique(pob_region$REGION) # todo ok
+# [1] "AMAZONAS"      "ANCASH"        "APURIMAC"      "AREQUIPA"      "AYACUCHO"     
+# [6] "CAJAMARCA"     "CALLAO"        "CUSCO"         "HUANCAVELICA"  "HUANUCO"      
+# [11] "ICA"           "JUNIN"         "LA LIBERTAD"   "LAMBAYEQUE"    "LIMA"         
+# [16] "LORETO"        "MADRE DE DIOS" "MOQUEGUA"      "PASCO"         "PIURA"        
+# [21] "PUNO"          "SAN MARTIN"    "TACNA"         "TUMBES"        "UCAYALI"
 
 ## Merge cases with populations
 region_values <- merge(casos_region, pob_region, by="REGION", 
@@ -120,6 +139,11 @@ region_values$incidencerate_100 <-(region_values$incidencia*100000)
 ## recode the names of the region_values data frame
 setnames(region_values, old = "REGION",new = "NAME_1")
 levels(region_values$NAME_1)
+# [1] "ANCASH"        "APURIMAC"      "AREQUIPA"      "AYACUCHO"      "CAJAMARCA"    
+# [6] "CALLAO"        "CUSCO"         "HUANCAVELICA"  "HUANUCO"       "ICA"          
+# [11] "JUNIN"         "LA LIBERTAD"   "LAMBAYEQUE"    "LIMA"          "LORETO"       
+# [16] "MADRE DE DIOS" "PASCO"         "PIURA"         "SAN MARTIN"    "TACNA"        
+# [21] "TUMBES"        "AMAZONAS"      "MOQUEGUA"      "PUNO"          "UCAYALI" 
 
 ## Create labels that does not occupy 
 region_values$NAME_LABEL <- region_values$NAME_1
@@ -157,41 +181,42 @@ tmp1 <- sh_peru
 tmp1_map <- tmp1
 tmp1_map@data <- join(sh_peru@data,region_values, by="NAME_1") # shp with data
 tmp1_map_df <- tmp1_map@data
-# tmp1_map@data$NUMERO_CASOS[is.na(tmp1_map@data$NUMERO_CASOS)]<-0
-# tmp1_map@data$incidencia[is.na(tmp1_map@data$incidencia)]<-0
-# tmp1_map@data$incidencerate_100[is.na(tmp1_map@data$incidencerate_100)]<-0
+tmp1_map@data$NUMERO_CASOS[is.na(tmp1_map@data$NUMERO_CASOS)]<-0
+tmp1_map@data$incidencia[is.na(tmp1_map@data$incidencia)]<-0
+tmp1_map@data$incidencerate_100[is.na(tmp1_map@data$incidencerate_100)]<-0
 tmp1_map_df <- tmp1_map@data
-# tmp1_map@data$brks <- cut(tmp1_map@data$incidencerate_100, 
-#                            breaks=c(-0.1, 0.0, 1, 2, 3, 4, 5, 6), 
-#                            labels=c("Sin casos reportados", "0 a 1", "1 a 2", "2 a 3", 
-#                                     "3 a 4", "4 a 5", "5 a 6"))
+tmp1_map@data$brks <- cut(tmp1_map@data$incidencerate_100, 
+                            breaks=c(-0.1, 0.0, 1, 2, 4, 6, 8, 10, 12, 20), 
+                            labels=c("0", "< 1", "1 a 2", 
+                                     "2 a 4", "4 a 6", "6 a 8",
+                                     "8 a 10", "10 a 12", "> 12"))
 names(tmp1_map@data) # check variables
 tmp1_map_df <- tmp1_map@data
 
 
 # Create the map qtm
-# col <- c("white", "ivory2", "lightcyan1", "lightblue2", #"lightskyblue", 
-#          "royalblue", #"blue2", 
-#          "navyblue", "black")
-
-col <- c("ivory2", "lightcyan1", "lightblue2", #"lightskyblue", 
+col <- c("white", "ivory2", "lightcyan1", "lightblue2", "lightskyblue", 
           "royalblue", "blue2", "navyblue", "black")
+
+# col <- c("ivory2", "lightcyan1", "lightblue2", #"lightskyblue", 
+#           "royalblue", "blue2", "navyblue", "black")
 # try snow2 too
 
 # interactive view
-tmap_mode("view")
+#tmap_mode("view")
 
 # crear mapa
-qtm <- qtm(tmp1_map, fill = "incidencerate_100", 
+qtm <- qtm(tmp1_map, #fill = "incidencerate_100", 
+           fill = "brks",
              fill.palette = col,
              fill.title="Incidencia por \n100,000 Hab") + 
   # tm_text("NAME_3", size="0.008", scale=1, root=4, 
   #         size.lowerbound = .1, bg.color="white", bg.alpha = .15, 
   #         legend.size.show = FALSE) +
     tm_text("NAME_LABEL",size = 0.5)+
-    tm_layout(paste("Casos COVID19 \npor región"), title.size = 1,
+    tm_layout(paste("Casos COVID19 \npor región"), title.size = .9,
               legend.title.size = 0.8, 
-            legend.text.size = .6, legend.bg.color = "white",
+            legend.text.size = .6, legend.bg.color = NA,
             legend.position = c("left","bottom")) #+ 
     #tm_view(set.view = c(lon = 15, lat = 48, zoom = 10))
    # tm_view(set.view = c(zoom = 10))
@@ -200,8 +225,9 @@ print(qtm)
 
 # RECUERDA CAMBIAR EL NOMBRE DEL ARCHIVO!
 tmap_save(qtm, 
-            paste("outputs_covid19/20200405_Dep_COVID19_Incidencerate",".png", sep=""), 
-           width=2300, height=1380)
+             paste("outputs_covid19/20200405_Dep_COVID19_Incidencerate_v2",
+                   ".png", sep=""), 
+            width=2300, height=1380)
 # Poner el # luego de guardar grafico
 
 
