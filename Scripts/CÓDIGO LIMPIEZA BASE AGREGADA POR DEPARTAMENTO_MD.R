c1c7a005b5932d2c511fddfcca9136ac4cf91244
arenica2 <arenica2@gmail.com>
2020-04-13 18:45:20 -0500

    update departamento por dia

--- Scripts/CÓDIGO LIMPIEZA BASE AGREGADA POR DEPARTAMENTO_MD.R
+++ Scripts/CÓDIGO LIMPIEZA BASE AGREGADA POR DEPARTAMENTO_MD.R
@@ -95,7 +95,7 @@ covid_dep2$CASOS[is.na(covid_dep2$CASOS)] <- 0
 # Save csv.
 # CAMBIA EL NOMBRE! NO TE OLVIDES!
 # write.csv(covid_dep2, "data/modificadas/covidPE_IND_20200412_MD_clean.csv")
-write.csv(covid_dep2, "data/modificadas/covidPE_IND_20200413_CA_clean.csv")
+write.csv(covid_dep2, "data/modificadas/covidPE_DEP_20200413_CA_clean.csv", row.names = FALSE)
 
 # Y PONLE UN # ANTES DE GUARDAR EL SCRIPT!
 
