1ca598ad67396a6c6dcd56041cecda313acfe283
Adriana <luque.lopez.adriana@gmail.com>
2020-04-30 19:58:06 -0500

    actualizacion de graficos 2020-04-30

--- Scripts/CÓDIGO LIMPIEZA BASE AGREGADA POR DIA_CA_MD.R
+++ Scripts/CÓDIGO LIMPIEZA BASE AGREGADA POR DIA_CA_MD.R
@@ -36,10 +36,11 @@ setwd("~/covid19")
 # covid_dia <- fread("~/covid19/data/crudas/covidPE_pordia_20200426_GF.csv")
 # covid_dia <- fread("~/covid19/data/crudas/covidPE_pordia_20200427_MD.csv")
 # covid_dia <- fread("~/covid19/data/crudas/covidPE_pordia_20200428_AL.csv")
-covid_dia <- fread("~/covid19/data/crudas/covidPE_pordia_20200429_GF.csv")
+# covid_dia <- fread("~/covid19/data/crudas/covidPE_pordia_20200429_GF.csv")
+covid_dia <- fread("~/covid19/data/crudas/covidPE_pordia_20200430_AL.csv")
 
 ## Checar base
-str(covid_dia) # 54 obs. of  14 variables
+str(covid_dia) # 55 obs. of  14 variables
 names(covid_dia)
 # [1] "FECHA"              "TOTAL_POSITIVOS"    "TOTAL_PRUEBAS"      "TOTAL_DESCARTADOS" 
 # [5] "POSITIVOS_DIA"      "PRUEBAS_DIA"        "DESCARTADOS_DIA"    "RECUPERADOS"       
@@ -81,8 +82,8 @@ table(covid_dia$FECHA)
 # 1          1          1          1          1          1          1 
 # 24/04/2020 25/03/2020 25/04/2020 26/03/2020 26/04/2020 27/03/2020 27/04/2020 
 # 1          1          1          1          1          1          1 
-# 28/03/2020 29/03/2020 30/03/2020 31/03/2020 
-# 1          1          1          1 
+# 28/03/2020 28/04/2020 29/03/2020 29/04/2020 30/03/2020 31/03/2020 
+# 1          1          1          1          1          1 
 
 
 #### separando caracteres en varias columnas ######
@@ -156,6 +157,6 @@ names(covid_dia)
 
 # Save csv.
 # CAMBIA EL NOMBRE! NO TE OLVIDES!
-# write.csv(covid_dia, "data/modificadas/covidPE_PORdia_20200429_GF_clean.csv")
+# write.csv(covid_dia, "data/modificadas/covidPE_PORdia_20200430_AL_clean.csv")
 # Y PONLE UN # ANTES DE GUARDAR EL SCRIPT!
-##
+###
--- Scripts/Peru_casos_pos_vs_pruebas_totales.R
+++ Scripts/Peru_casos_pos_vs_pruebas_totales.R
@@ -53,7 +53,8 @@ d=data.frame(Fecha=as.Date(c("2020-03-06", "2020-03-11", "2020-03-12", "2020-03-
 # acumulado<-fread("~/covid19/data/modificadas/covidPE_PORdia_20200426_GF_clean.csv")
 # acumulado<-fread("~/covid19/data/modificadas/covidPE_PORdia_20200427_MD_clean.csv")
 # acumulado<-fread("~/covid19/data/modificadas/covidPE_PORdia_20200428_AL_clean.csv")
-acumulado<-fread("~/covid19/data/modificadas/covidPE_PORdia_20200429_GF_clean.csv")
+# acumulado<-fread("~/covid19/data/modificadas/covidPE_PORdia_20200429_GF_clean.csv")
+acumulado<-fread("~/covid19/data/modificadas/covidPE_PORdia_20200430_AL_clean.csv")
 
 
 setnames(acumulado,"TOTAL_POSITIVOS","Casos positivos")
@@ -69,7 +70,7 @@ acumulado1<-acumulado1 %>%
 setwd("~/covid19/outputs_covid19/")
 # 
 #Cambiar el nombre
-png(filename="20200429_pruebas_vs_positivos_COVID19.png", width=1100, height=600)
+png(filename="20200430_pruebas_vs_positivos_COVID19.png", width=1100, height=600)
 ggplot() + 
   geom_line(data = acumulado1, aes(Fecha, value, colour=variable,group=variable),alpha =1.6) +
   scale_colour_manual(values=c("red", "blue"))+
