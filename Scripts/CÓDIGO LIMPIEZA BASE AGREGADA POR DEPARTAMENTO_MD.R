55f6694ad36935fda531ee62e7481ca915aaee56
arenica2 <arenica2@gmail.com>
2020-05-07 18:00:54 -0500

    update maps and scripts 22020-05-07

--- Scripts/CÓDIGO LIMPIEZA BASE AGREGADA POR DEPARTAMENTO_MD.R
+++ Scripts/CÓDIGO LIMPIEZA BASE AGREGADA POR DEPARTAMENTO_MD.R
@@ -31,10 +31,11 @@ setwd("~/covid19")
 # covid_dep <- fread("~/covid19/data/crudas/covidPE_DEP_20200501_AL.csv")
 # covid_dep <- fread("~/covid19/data/crudas/covidPE_DEP_20200502_MD.csv")
 # covid_dep <- fread("~/covid19/data/crudas/covidPE_DEP_20200503_GF.csv")
-   covid_dep <- fread("~/covid19/data/crudas/covidPE_DEP_202005005_CA.csv")
-
+   # covid_dep <- fread("~/covid19/data/crudas/covidPE_DEP_202005005_CA.csv")
+   covid_dep <- fread("~/covid19/data/crudas/covidPE_DEP_202005007_CA.csv")
+   
 ## Checar base
-str(covid_dep) # 61 obs. of  31 variables
+str(covid_dep) # 64 obs. of  31 variables
 names(covid_dep)
 # [1] "Día"         "Fecha"       "LIM"         "ARE"         "HUC"         "ICA"        
 # [7] "CUS"         "PIU"         "LAM"         "CAL"         "LAL"         "ANC"        
@@ -48,7 +49,7 @@ names(covid_dep)
 #Recuerde aqui quitar la ultima columna y las columna y fila de "totales"
 # Ma?ana 04/05 hay que sacar la fila 60.
 # Dia 03/05 se sac[o] la fila 59.
-covid_dep <- covid_dep[-61,-c(1,28:31)] #OJO
+covid_dep <- covid_dep[-63,-c(1,28:31)] #OJO
 # Mas adelante  Sacar los "S1N0 del nombre de la variable
 
 head(covid_dep)
@@ -70,27 +71,24 @@ sapply(covid_dep, class)
 
 ### variableby variable
 table(covid_dep$Fecha)
-# 01/04/2020 01/05/2020 02/04/2020 02/05/2020 03/04/2020 03/05/2020 
-# 1          1          1          1          1          1 
-# 04/04/2020 04/05/2020 05/04/2020 06/03/2020 06/04/2020 07/03/2020 
-# 1          1          1          1          1          1 
-# 07/04/2020 08/03/2020 08/04/2020 09/03/2020 09/04/2020 10/03/2020 
-# 1          1          1          1          1          1 
-# 10/04/2020 11/03/2020 11/04/2020 12/03/2020 12/04/2020 13/03/2020 
-# 1          1          1          1          1          1 
-# 13/04/2020 14/03/2020 14/04/2020 15/03/2020 15/04/2020 16/03/2020 
-# 1          1          1          1          1          1 
-# 16/04/2020 17/03/2020 17/04/2020 18/03/2020 18/04/2020 19/03/2020 
-# 1          1          1          1          1          1 
-# 19/04/2020 20/03/2020 20/04/2020 21/03/2020 21/04/2020 22/03/2020 
-# 1          1          1          1          1          1 
-# 22/04/2020 23/03/2020 23/04/2020 24/03/2020 24/04/2020 25/03/2020 
-# 1          1          1          1          1          1 
-# 25/04/2020 26/03/2020 26/04/2020 27/03/2020 27/04/2020 28/03/2020 
-# 1          1          1          1          1          1 
+# 01/04/2020 01/05/2020 02/04/2020 02/05/2020 03/04/2020 03/05/2020 04/04/2020 
+# 1          1          1          1          1          1          1 
+# 04/05/2020 05/04/2020 05/05/2020 06/03/2020 06/04/2020 06/05/2020 07/03/2020 
+# 1          1          1          1          1          1          1 
+# 07/04/2020 08/03/2020 08/04/2020 09/03/2020 09/04/2020 10/03/2020 10/04/2020 
+# 1          1          1          1          1          1          1 
+# 11/03/2020 11/04/2020 12/03/2020 12/04/2020 13/03/2020 13/04/2020 14/03/2020 
+# 1          1          1          1          1          1          1 
+# 14/04/2020 15/03/2020 15/04/2020 16/03/2020 16/04/2020 17/03/2020 17/04/2020 
+# 1          1          1          1          1          1          1 
+# 18/03/2020 18/04/2020 19/03/2020 19/04/2020 20/03/2020 20/04/2020 21/03/2020 
+# 1          1          1          1          1          1          1 
+# 21/04/2020 22/03/2020 22/04/2020 23/03/2020 23/04/2020 24/03/2020 24/04/2020 
+# 1          1          1          1          1          1          1 
+# 25/03/2020 25/04/2020 26/03/2020 26/04/2020 27/03/2020 27/04/2020 28/03/2020 
+# 1          1          1          1          1          1          1 
 # 28/04/2020 29/03/2020 29/04/2020 30/03/2020 30/04/2020 31/03/2020 
 # 1          1          1          1          1          1 
-# # 1 
 #### separando caracteres en varias columnas ######
 # covid_ind$FECHA2 <- covid_ind$FECHA
 # covid_ind <- separate(covid_ind,
@@ -148,7 +146,8 @@ covid_dep2$CASOS[is.na(covid_dep2$CASOS)] <- 0
 # write.csv(covid_dep2, "data/modificadas/covidPE_DEP_20200501_AL_clean.csv", row.names = FALSE)
 # write.csv(covid_dep2, "data/modificadas/covidPE_DEP_20200502_MD_clean.csv", row.names = FALSE)
 # write.csv(covid_dep2, "data/modificadas/covidPE_DEP_20200503_GF_clean.csv", row.names = FALSE)
-# write.csv(covid_dep2, "data/modificadas/covidPE_DEP_2020050_CA_clean.csv", row.names = FALSE)
+# write.csv(covid_dep2, "data/modificadas/covidPE_DEP_20200505_CA_clean.csv", row.names = FALSE)
+write.csv(covid_dep2, "data/modificadas/covidPE_DEP_20200507_CA_clean.csv", row.names = FALSE)
 
 # Y PONLE UN # ANTES DE GUARDAR EL SCRIPT!
 
